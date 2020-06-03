7fb05b431a8a1db7aa952b1d45109d248ec2eae7
Alex F <frutkinalexander@gmail.com>
2020-06-03 18:59:00 -0400

    Basic code structure established

--- calculator.py
+++ calculator.py
@@ -3,4 +3,16 @@ import sys
 
 # this is a program written in the summer of 2020 by Alex Frutkin
 
-print("PyCalculator, written in 2020 by Alex Frutkin")
+operation = ""
+firstNumber = 0.0
+secondNumber = 0.0
+
+print("PyCalculator, written in 2020 by yummyDev")
+
+while operation != "q" and operation != "Q":
+    operation = input("What do you want to do? (+, -, /, *, q(uit)): ")
+    if operation == "q" or operation == "Q":
+        break
+    print("Ok, you have selected the following operation: " + operation)
+    firstNumber = float(input("What is the first number you want to use? "))
+    secondNumber = float(input("What is the second number you want to use? "))
