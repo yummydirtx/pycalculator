3a1fbc54c41cf4f4be387d709b8a44bccc1731a5
Alex F <frutkinalexander@gmail.com>
2020-06-03 17:50:07 -0400

    fixed the interpreter path

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-# this is a program written in the summer of 2020 by Alex Frutkin
-
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
+
+# this is a program written in the summer of 2020 by Alex Frutkin
 
 print("PyCalculator, written in 2020 by Alex Frutkin")
