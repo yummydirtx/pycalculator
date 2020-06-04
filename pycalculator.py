76f0a3f27fa1dcabdd6fc901a37be4952ba0298c
Alex F <frutkinalexander@gmail.com>
2020-06-03 21:03:08 -0400

    added the functions

--- pycalculator.py
+++ pycalculator.py
@@ -11,8 +11,19 @@ print("PyCalculator, written in 2020 by yummyDev")
 
 while operation != "q" and operation != "Q":
     operation = input("What do you want to do? (+, -, /, *, q(uit)): ")
+    if operation != "+" and operation != "-" and operation != "/" and operation != "*" and operation != "q" and operation != "Q":
+        print("Error: " + operation + " is not recognized.")
+        break
     if operation == "q" or operation == "Q":
         break
     print("Ok, you have selected the following operation: " + operation)
     firstNumber = float(input("What is the first number you want to use? "))
     secondNumber = float(input("What is the second number you want to use? "))
+    if operation == "+":
+        print("The answer is " + str((firstNumber + secondNumber)))
+    if operation == "-":
+        print("The answer is " + str((firstNumber - secondNumber)))
+    if operation == "/":
+        print("The answer is " + str((firstNumber / secondNumber)))
+    if operation == "*":
+        print("The answer is " + str((firstNumber * secondNumber)))
